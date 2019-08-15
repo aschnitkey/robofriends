2b4a9aedd8333c856f6ea64b4f81612d5db62777
aschnitkey <aschnitkey@gmail.com>
2019-08-15 14:59:03 -0400

    Changed from Robots to Monsters.....for fun

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({name, email, id}) {
     return(
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-            <img src={`https://robohash.org/${id}?200x200`} alt="Test"/>
+            <img src={`https://robohash.org/${id}?set=set2&size=250x250`} alt="Test"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({searchField, searchChange}) => {
     return(
         <div className='pa2'>
-            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Robots..." name="search" onChange={searchChange} />
+            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Monsters..." name="search" onChange={searchChange} />
         </div>
     );
 }
